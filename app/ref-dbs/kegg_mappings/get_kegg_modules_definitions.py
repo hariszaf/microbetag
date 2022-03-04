4d6ae837499f0fc7a459309742934e94b1c0eb84
hariszaf <haris-zaf@hcmr.gr>
2022-03-04 19:25:40 +0100

    get multiple alternative cases (definitions) when available for a module

--- app/ref-dbs/kegg_mappings/get_kegg_modules_definitions.py
+++ app/ref-dbs/kegg_mappings/get_kegg_modules_definitions.py
@@ -3,39 +3,65 @@
 import time, sys
 import requests
 
+
+def write_definition(line, md, iter):
+
+   if iter > 1: 
+      definition = line.strip()
+      print("HERE WE GO:  ", definition)
+      with open("module_definitions.tsv", "a") as f:
+            f.write(md + "_" + str(iter) + "\t" + definition + "\n")
+
+   else:
+      definition = line.split("  ")[1:][0][:-1]
+      definition = ';'.join(definition.split(" "))
+      with open("module_definitions.tsv", "a") as f:
+            f.write(md + "\t" + definition + "\n")
+
+   return True
+
+
+
 all_modules_ids     = []
-# modules_definitions = {}
 
 for line in open("kegg_terms_per_module.tsv", "r"): 
    md_id = line.split("\t")[0]
    if md_id not in all_modules_ids: 
       all_modules_ids.append(md_id)
 
-
 print("# of modules: ", len(all_modules_ids))
 
 for md in all_modules_ids:
-
-   print(md)
    
+   print(md)
+
    url       = "http://rest.kegg.jp/get/" + md
    response   = requests.get(url)
-   # print(response.content)
 
    with open("response.txt", "wb") as f:
       f.write(response.content)
-      
-   for line in open("response.txt", "r"):
-      if "DEFINITION" in line:
-         definition = line.split("  ")[1:][0][:-1]
-         definition = ';'.join(definition.split(" "))
-         # modules_definitions[md] = definition
 
-         with open("module_definitions.tsv", "a") as f:
-               f.write(md + "\t" + definition + "\n")
+   switch      = False
+   num_of_defs = 0 
+
+   for line in open("response.txt", "r"):
+
+      if "DEFINITION" in line:
+         switch      = True
+         num_of_defs = 1
+         write_definition(line, md, num_of_defs)
+         continue
+
+      if "ORTHOLOGY" in line: 
+         switch = False
+      
+      if switch: 
+         num_of_defs += 1
+         write_definition(line, md, num_of_defs)
    
    time.sleep(0.5)
          
+
 
 
 # Definition	The definition of the module as a logcail expression of K numbers. 
