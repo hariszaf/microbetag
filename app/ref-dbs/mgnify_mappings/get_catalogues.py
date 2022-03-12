af7344d8090a6194169afeada271cecd629d20db
hariszaf <haris-zaf@hcmr.gr>
2022-03-12 18:03:48 +0100

    revised version

--- app/ref-dbs/mgnify_mappings/get_catalogues.py
+++ app/ref-dbs/mgnify_mappings/get_catalogues.py
@@ -12,6 +12,12 @@ import random
 from jsonapi_client import Session # pip install jsonapi-client
 import logging
 from subprocess import Popen, PIPE
+import sys
+
+
+# Get the one level up path 
+def get_parent_dir(directory):
+    return os.path.dirname(directory)
 
 # Link biosample or ncbi genome id to ncbi taxonomy id
 def efetch_using_ncbi_genome_id(genome_id, db):
@@ -139,23 +145,155 @@ def map_gtdb_lineage_to_ncbi_tax_id(metadata_dict):
 # Get the KOs assigned to the MAG
 def get_KOs(metadata_dict): 
 
+   API_BASE = "http://ftp.ebi.ac.uk/pub/databases/metagenomics/mgnify_genomes/"
+   ref_dbs_dir = get_parent_dir(os.getcwd())
+
+   kos_per_module_reference_file = open(ref_dbs_dir + "/kegg_mappings/kegg_terms_per_module.tsv", "r")
+
+   kegg_terms_per_module_reference = {}
+   for line in kos_per_module_reference_file: 
+      module = line.split("\t")[0]
+      ko     = line.split("\t")[1][:-1]
+      if module not in kegg_terms_per_module_reference:
+         kegg_terms_per_module_reference[module] = [ko]
+      else:
+         kegg_terms_per_module_reference[module].append(ko)
+
+   num_of_mags_to_parse   = len(metadata_dict)
+   counter                = 0 
+   non_species_level_mags = 0 
+   print("Total number of MAGs to parse: ", num_of_mags_to_parse)
+   errors = []
+
+   # Parse metadata file per MAG
    for mag, metadata in metadata_dict.items():
-      # print(mag)
-      API_BASE = "http://ftp.ebi.ac.uk/pub/databases/metagenomics/mgnify_genomes/"
-      
+
+      counter += 1
+      ncbi_tax_id = metadata_dict[mag]['ncbi_tax_id']      
+      print("\n\n ~~~~\n\n ncbi tax id: ", ncbi_tax_id)
+      if ncbi_tax_id == "":
+         continue
+
+      dir_for_ncbi_id          = ref_dbs_dir + "/mgnify_catalogues/" + ncbi_tax_id
+      mag_data                 = dir_for_ncbi_id + "/" + mag + "_annotation.tsv"
+      file_mags_kos_per_module = dir_for_ncbi_id + "/" + mag + "_kos_related_to_mos.json"
+
+      if counter % 100 == 0:
+         print(counter, " MAGs have been parsed out of ", num_of_mags_to_parse)
+
+
+      if os.path.isdir(dir_for_ncbi_id) == False:
+         os.mkdir(dir_for_ncbi_id)
+         sec = random.uniform(5, 15)  # make it 20, 30 for many 
+         print(sec)
+         time.sleep(sec)
+
+      else:
+         
+         if os.path.exists(file_mags_kos_per_module):
+            continue
+         #    links_file = open(file_mags_kos_per_module, "r")
+         #    links      = json.load(links_file)
+         #    dic        = {}
+         #    mag_kos    = open(dir_for_ncbi_id + "/" + ncbi_tax_id + "_kos.tsv", "w")            
+         #    for mo, kos in links.items():            
+         #       for ko in kos: 
+         #          if mo not in dic: 
+         #             dic[mo] = {}
+         #             dic[mo][str(len(dic[mo]))] = ko
+         #          else:
+         #             dic[mo][str(len(dic[mo]))] = ko
+
+         #    out_file = open(file_mags_kos_per_module, "w")
+         #    json.dump(dic, out_file, indent = 6)
+         #    out_file.close()
+
+         # else:
+
+         #    try:
+         #       annotations = open(mag_data, "r")
+         #       next(annotations)
+
+         #       for line in annotations: 
+
+         #          mag_kos = line.split("\t")[11]
+         #          mag_kos = mag_kos.split(",")
+
+         #          for module, kos in kegg_terms_per_module_reference.items():
+
+         #             for ko in mag_kos:
+
+         #                if ko in kos: 
+         #                   mag_kos_output.write(module + "\t" + ko + "\n")
+
+         #                   if module not in mag_kos_per_module:
+         #                      mag_kos_per_module[module] = {}
+         #                      mag_kos_per_module[module]['1'] = ko
+         #                   else:
+         #                      mag_kos_per_module[module][str(len(mag_kos_per_module[module]) +1)] = ko
+
+         #       out_file = open(file_mags_kos_per_module, "w")
+         #       json.dump(mag_kos_per_module, out_file, indent = 6)
+         #       out_file.close()
+
+         #    except:
+         #       errors.append([ncbi_tax_id, mag])
+
+         # continue
+
+      mag_kos_per_module = {}
+
       catalogue = "-".join(metadata_dict[mag]['catalogue'].split("-")[:-2])
+      version   = ".".join(metadata_dict[mag]['catalogue'].split("-")[-2:])
+      if "." in mag: 
+         intermidate = mag.split(".")[0][:-2]
+      else:
+         intermidate = mag[:-2]
+      url       = API_BASE + catalogue + "/" + version + "/species_catalogue/" +\
+                   intermidate + "/" + mag + "/" + "genome/" + mag + "_eggNOG.tsv"
+      if ncbi_tax_id == "2049049":
+         print(mag, "\t", url)
 
-      version   = ".".join(metadata_dict[mag]['catalogue'].split("-")[-2:])  
-      
-      url = API_BASE + catalogue + "/" + version + "/species_catalogue/" + mag[:-2] + "/" + mag + "/" + "genome/" + mag + "_eggNOG.tsv"
-      
-      r = requests.get(url)
-      print(r.text)
+      try:
 
-      sys.exit(0)
+         r = requests.get(url, allow_redirects=True)
+         open(mag_data, 'wb').write(r.content)
+         annotations = open(mag_data, "r")
+         next(annotations)
+         mag_kos_output  = open(dir_for_ncbi_id + "/" + mag + "_kos_related_to_mos.tsv", "w")
 
+         for line in annotations: 
 
+            mag_kos = line.split("\t")[11]
+            mag_kos = mag_kos.split(",")
 
+            for module, kos in kegg_terms_per_module_reference.items():
+
+               for ko in mag_kos:
+
+                  if ko in kos: 
+                     mag_kos_output.write(module + "\t" + ko + "\n")
+
+                     if module not in mag_kos_per_module:
+                        mag_kos_per_module[module] = {}
+                        mag_kos_per_module[module]['1'] = ko
+                     else:
+                        mag_kos_per_module[module][str(len(mag_kos_per_module[module]) +1)] = ko
+
+         out_file = open(file_mags_kos_per_module, "w")
+         json.dump(mag_kos_per_module, out_file, indent = 6)
+         out_file.close()
+
+      except:
+         print("pali mas gamise")
+         errors.append([url, mag])
+
+   print("Number of non species level MAGs ", non_species_level_mags)
+
+   error_files = open("errors.tsv", "w")
+   for error in errors:
+      error_files.write(error[0] + "\t" +error[1] + "\n")
+   return errors
 
 ## STEP A
 # genomes_metadata = get_quality_mgnify_mags(90, 5)
@@ -165,30 +303,28 @@ def get_KOs(metadata_dict):
 # initial_mag_dic.close()
 
 
-## STEP B
+# ## STEP B
 init_file       = open("genomes_metadata.json", "r")
 init_dic        = json.load(init_file)
 
 mags_with_ncbi_ids = map_gtdb_lineage_to_ncbi_tax_id(init_dic)
 
+mgnify_unique = [] # 1,763 unique NCBI IDs
+for mag in mags_with_ncbi_ids: 
+   ncbi_id = mags_with_ncbi_ids[mag]['ncbi_tax_id']
+   if ncbi_id in mgnify_unique: 
+      continue 
+   else:
+      mgnify_unique.append(ncbi_id)
 
-get_KOs(mags_with_ncbi_ids)
+kegg_genomes = os.listdir(path='../kegg_genomes/') # 6,950 genomes
+TOTAL_UNIQUE = list(set(mgnify_unique + kegg_genomes))
+print(len(TOTAL_UNIQUE))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+out_file = open("genomes_metadata_ncbi_ids.json", "w")
+json.dump(mags_with_ncbi_ids, out_file, indent = 6)
+out_file.close()
+print(get_KOs(mags_with_ncbi_ids))
 
 
 """
