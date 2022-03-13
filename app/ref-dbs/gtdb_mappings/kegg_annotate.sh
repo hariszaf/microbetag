a40dc96421917b527dc39fb02a6d65bf43c4d7cd
hariszaf <haris.zafr@gmail.com>
2022-03-13 20:52:07 +0200

    minor

--- app/ref-dbs/gtdb_mappings/kegg_annotate.sh
+++ app/ref-dbs/gtdb_mappings/kegg_annotate.sh
@@ -4,12 +4,13 @@ BASE="/home/haris/Desktop/programs/"
 
 cd $BASE/kofam_scan/
 
-for faa in $BASE/microbetag/app/ref-dbs/gtdb_genomes/test/*; do 
+for faa in $BASE/microbetag/app/ref-dbs/gtdb_genomes/genomes_faa/*; do 
 
-    echo $faa ; date; 
-    #echo "${faa##*/}"
+    echo $faa ; 
+    date;
     ./exec_annotation -f mapper -o $BASE/microbetag/app/ref-dbs/gtdb_genomes/annotations/"${faa##*/}".tsv $faa;
-
+    mv $faa $BASE/microbetag/app/ref-dbs/gtdb_genomes/tmp
+    date;
 done
 
 
