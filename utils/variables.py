df4c4199924c6500f667ae8989ff6eafcba217ca
hariszaf <haris-zaf@hcmr.gr>
2022-03-17 00:20:02 +0100

    add kegg paths

--- utils/variables.py
+++ utils/variables.py
@@ -6,11 +6,11 @@ Global variables
 import os
 
 
-BASE                  = os.path.dirname(os.getcwd())
-REF_DBS               = os.path.join(BASE, "ref-dbs")
-SILVA_SPECIES_NCBI_ID = os.path.join(REF_DBS, "silva/species_names_to_ncbi_id.tsv")
-
-
+BASE                        = os.path.dirname(os.getcwd())
+REF_DBS                     = os.path.join(BASE, "ref-dbs")
+SILVA_SPECIES_NCBI_ID       = os.path.join(REF_DBS, "silva/species_names_to_ncbi_id.tsv")
+KMODULES_DEFINITIONS        = os.path.join(REF_DBS, "kegg_mappings/module_definitions.tsv")
+KMODULES_DEFINITIONS_PARSED = os.path.join(REF_DBS, "kegg_mappings/module_definition_map.json")
 
 
 
