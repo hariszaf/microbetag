9a48014ee745b1f314f570bb35f398337ecc6442
hariszaf <haris-zaf@hcmr.gr>
2022-03-17 00:18:41 +0100

    init global vars file

---
+++ utils/variables.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""
+Global variables
+"""
+import os
+
+
+BASE                  = os.path.dirname(os.getcwd())
+REF_DBS               = os.path.join(BASE, "ref-dbs")
+SILVA_SPECIES_NCBI_ID = os.path.join(REF_DBS, "silva/species_names_to_ncbi_id.tsv")
+
+
+
+
+
+
