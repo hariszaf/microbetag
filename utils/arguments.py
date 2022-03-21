a31a1a8e731c599e847872160a4711b6aeeb6b4c
hariszaf <haris-zaf@hcmr.gr>
2022-03-22 00:20:21 +0100

    change from argparse to yaml

--- utils/arguments.py
+++ utils/arguments.py
@@ -6,70 +6,9 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-# parser.add_argument('-conf', '--configuration_file', metavar='configuration_file', dest='conf',
-#                     required = True,
-#                     help='YAML configuration file with the parameter values')
-
-"""
-FlashWeave related paramters
-"""
-parser.add_argument('-el', '--edge_list', metavar='edge_list', dest='el',
-                    type=str, required = False,
-                    help='edge list file with the associations present in a co-occurrence network. If available, this should be a 2 column tab separated file.')
-
-
-
-
-
-"""
-FAPROTAX related parameters
-"""
-parser.add_argument('-i', '--otu_table', metavar = 'input_otu_table', dest = 'i',
-                    type = str, required = True,
-                    help = 'OTU table with taxonomies assigned using the Silva database. In case other database was used, the OTU table needs to have an extra column specifying the NCBI Taxonomy id of the assignments that are up to the species level')
-
-# If none, the last column of the OTU table will be used as such 
-parser.add_argument('-t', '--taxonomy_column_name', metavar='taxonomy_column', dest='t',
-                    type=str, required = False,
-                    help='exact name of the column denoting the taxonomy assignment in the OTU table')
-
-# If none, the first column of the OTU table will be used as such 
-parser.add_argument('-c', '--otu_identifier_column', metavar='taxonomy_column', dest='c',
-                    type=str, required = False,
-                    help='exact name of the column denoting the taxonomy assignment in the OTU table')
-
-# If none, the first character of the last line of the non taxa lines will be used
-parser.add_argument('-com', '--comments_character', metavar='comments_character', dest='com',
-                    type=str, required = False,
-                    help='the character denoting commented lines on the OTU table')
-
-# If none, the first character of the last line of the non taxa lines will be used
-parser.add_argument('-ch', '--commented_header', dest='ch',
-                    action='store_true', required = False, default = False,
-                    help = 'True')
-
-
-"""
-BugBase related parameters
-"""
-
-# Metadata table as described in the BugBase documentation
-parser.add_argument('-m', '--mapping_file', metavar = 'mapping_file', dest = 'm',
-                    type = str, required = False,
-                    help = 'Metadata table as described in the BugBase documentation. See: https://bugbase.cs.umn.edu/documentation.html')
-
-
-# If none, the output directory will be built in the current working directory
-parser.add_argument('-o', '--outdir', metavar='output_dir', dest='o',
-                    type = str, required = False, default = "output",
-                    help = 'output directory')
-
-# By default, microbetag assumes the OTU table is in Silva v.132
-parser.add_argument('-is', '--is_silva', metavar='is_silva_taxonomy', dest='is',
-                    type = str, required = False, default = "True",
-                    help = 'threads that will be used')
-
-
+parser.add_argument('-conf', '--configuration_file', metavar ='configuration_file', dest = 'conf',
+                    required = True,
+                    help='YAML configuration file with the parameter values')
 
 """
 Parse arguments
--- utils/variables.py
+++ utils/variables.py
@@ -1,78 +1,78 @@
 """
 Global variables
 """
-import os
-import sys # even if not needed here, it is used on the microbetag.py script
 
-# A relative import specifies the resource to be imported relative to the current location; 
-# the location where the import statement is.
-# A single dot means that the module or package referenced is in the same directory as the current location. 
-# Two dots mean that it is in the parent directory of the current location—that is, the directory above. 
-# Three dots mean that it is in the grandparent directory, and so on.
-# For more: 
-# # For more have a look at: https://realpython.com/absolute-vs-relative-python-imports/#syntax-and-practical-examples_1
-# Remove . if you want to test the script on its own - not calling it from the microbetag.py script
+__author__  = 'Haris Zafeiropoulos'
+__email__   = 'haris-zaf@hcmr.gr'
+__status__  = 'Development'
+__license__ = 'GPLv3'
+
+
+import os
+import sys 
+import yaml
 from .arguments import *
 
+
+# Read configuration file
+with open(args.conf, "r") as ymlfile:
+   cfg = yaml.load(ymlfile, Loader = yaml.FullLoader)
+
+
 BASE      = os.getcwd() 
-OUT_DIR   = args.o
 
-EDGE_LIST = args.el
+if cfg['approach']['container']:
+   IO_PATH   = "/mnt/"
 
-OTU_TABLE = args.i
-TAX_COL   = args.t
-OTU_COL   = args.c
-COM_CHAR  = args.com
-if args.ch != False: 
+else:
+   IO_PATH   = cfg['approach']['io_path']
+
+
+OTU_TABLE = os.path.join(IO_PATH, cfg['otu_table'])
+
+OUT_DIR   = os.path.join(IO_PATH, cfg['output_directory'])
+
+
+TAX_COL   = cfg['taxonomy_column_name']
+OTU_COL   = cfg['otu_identifier_column']
+COM_CHAR  = cfg['comments_character']
+
+if cfg['column_names_are_in'] == True: 
    COM_HEAD  = '"' + 'last_comment_line' + '"'
 
 
-if args.t is None:
-   with open(OTU_TABLE) as f:
-      lines = f.readlines()
-   lengths = set()
-   for line in lines: 
-      line = line.split("\t")
-      lengths.add(len(line))
-   max_num_of_cells = max(lengths)
-   for line in lines: 
-      line = line.split("\t")
-      if len(line) == max_num_of_cells:
-         if TAX_COL is None:
-            TAX_COL = line[-1].rstrip()
-            # TAX_COL = '"' + line[-1].rstrip() + '"'
-         if OTU_COL is None: 
-            OTU_COL = line[0].rstrip()
-         break
-      if args.com is None:
-         COM_CHAR = '"' + line[0][0] + '"'
+EDGE_LIST = cfg['edge_list']
+
+METADATA_FILE = cfg['metadata_file']
 
 
+
+
+# Paths
 REF_DBS                     = os.path.join(BASE, "ref-dbs")
 SILVA_SPECIES_NCBI_ID       = os.path.join(REF_DBS, "silva/species_names_to_ncbi_id.tsv")
 KMODULES_DEFINITIONS        = os.path.join(REF_DBS, "kegg_mappings/module_definitions.tsv")
 KMODULES_DEFINITIONS_PARSED = os.path.join(REF_DBS, "kegg_mappings/module_definition_map.json")
-
-
 TOOLS                       = os.path.join(BASE, "tools")
 
+# FlashWeave
 FLASHWEAVE_SCRIPT           = os.path.join(TOOLS, "flashweave/flashweave.jl")
 FLASHWEAVE_OUTPUT_DIR       = os.path.join(OUT_DIR, "flashweave")
 FLASHWEAVE_TMP_INPUT        = os.path.join(FLASHWEAVE_OUTPUT_DIR, "otu_table_flashweave_format.tsv")
-# print("#@#$@#$ -->", FLASHWEAVE_TMP_INPUT)
+FLASHWEAVE_EDGELIST         = os.path.join(FLASHWEAVE_OUTPUT_DIR, "network_output.edgelist")
 
+# FAPROTAX
 FAPROTAX_SCRIPT             = os.path.join(TOOLS, "faprotax/collapse_table.py")
 FAPROTAX_DB                 = os.path.join(TOOLS, "faprotax/FAPROTAX.txt")
-
-
-
-
-
-
 
 FAPROTAX_OUTPUT_DIR         = os.path.join(OUT_DIR, "faprotax")
 FAPROTAX_FUNCT_TABLE        = os.path.join(FAPROTAX_OUTPUT_DIR, "functional_otu_table.tsv")
 FAPROTAX_SUB_TABLES         = os.path.join(FAPROTAX_OUTPUT_DIR, "sub_tables")
+
+# BugBase
+BUGBASE_OUTPUT              = os.path.join(OUT_DIR, "bugbase")
+BUGBASE_OPTIONAL            = cfg['bugbase_opt']
+
 
 
 
