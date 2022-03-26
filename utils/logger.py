0930ea71fec81f0a2e8d16eb2fdaf687775269a0
hariszaf <haris-zaf@hcmr.gr>
2022-03-26 22:52:54 +0100

    logger

---
+++ utils/logger.py
@@ -0,0 +1,13 @@
+"""
+Logging 
+"""
+
+import logging
+
+# Set logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter(
+    '%(asctime)s [%(levelname)-8s] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S')
+
