6333ac8beb8c58d168f94fc155a6d995ae5a0e6d
hariszaf <haris-zaf@hcmr.gr>
2022-04-04 13:06:43 +0200

    set paths to make 2-col gtdb files

--- mappings/gtdb_mappings/gtdb_annotations_per_module.py
+++ mappings/gtdb_mappings/gtdb_annotations_per_module.py
@@ -18,7 +18,7 @@ mappings        = {}
 
 metadata_file = open("GTDB_QUALITY_REPRESENTATIVE_GENOMES", "r")
 
-for filename in os.listdir("../gtdb_genomes/ko_annotations"):
+for filename in os.listdir("../../ref-dbs/gtdb_genomes/all_kofam_scan/"):
     accession = '_'.join([filename.split("_")[0], filename.split("_")[1]])
     accessions.append(accession)
     accession_files[accession] = filename
@@ -36,14 +36,14 @@ for line in metadata_file:
         mappings[ncbi_genome_accession_id] = ncbi_taxonomy_id
         uniq_ncbi_ids.add(ncbi_taxonomy_id)
 
-	# replace testingpy with ko_annotationsal 
-        ncbi_path = os.path.join("../gtdb_genomes/testingpy/", ncbi_taxonomy_id)
+	# replace all_kofam_scan with ko_annotations  
+        ncbi_path = os.path.join("../../ref-dbs/gtdb_genomes/all_kofam_scan/", ncbi_taxonomy_id)
 
         if os.path.isdir(ncbi_path) == False:
             os.mkdir(ncbi_path)
 
-        faa_file        = "../gtdb_genomes/testingpy/" + accession_files[ncbi_genome_accession_id]
-        kegg_annot_file = "../gtdb_genomes/testingpy/" + str(ncbi_taxonomy_id) + "/" + accession_files[ncbi_genome_accession_id]
+        faa_file        = "../../ref-dbs/gtdb_genomes/all_kofam_scan/" + accession_files[ncbi_genome_accession_id]
+        kegg_annot_file = "../../ref-dbs/gtdb_genomes/all_kofam_scan/" + str(ncbi_taxonomy_id) + "/" + accession_files[ncbi_genome_accession_id]
 
         os.replace(faa_file, kegg_annot_file)
 
