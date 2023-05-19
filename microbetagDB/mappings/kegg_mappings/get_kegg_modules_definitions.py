268886f497af6baaa60f7563b007a63c61258f65
Haris Zafeiropoulos <haris.zafeiropoulos@kuleuven.be>
2023-05-19 14:02:06 +0200

    fixed module parsing #8

--- microbetagDB/mappings/kegg_mappings/get_kegg_modules_definitions.py
+++ microbetagDB/mappings/kegg_mappings/get_kegg_modules_definitions.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
 
+"""
+This script takes as input the kegg_terms_per_module.tsv file;
+that is actually the results of the command:
+wget https://rest.kegg.jp/link/ko/md
+
+It makes a list of all the KEGG modules and then retrieves from the KEGG portal 
+their definitions, to save them in the module_definitions.tsv file like this:
+
+md:M00006       (K13937,((K00036,K19243);(K01057,K07404)));K00033
+
+The "definition" of a module is a logcail expression of K numbers. 
+Comma (,) separated K numbers indicate alternatives. 
+Plus signs (+) are used to represent a complex and a minus sign (-) denotes a non-essential component in the complex.
+"""
+
 import time, sys
 import requests
-
 
 def write_definition(line, md, iter):
 
    if iter > 1: 
       definition = line.strip()
-      print("HERE WE GO:  ", definition)
       with open("module_definitions.tsv", "a") as f:
             f.write(md + "_" + str(iter) + "\t" + definition + "\n")
 
@@ -21,20 +34,15 @@ def write_definition(line, md, iter):
    return True
 
 
-
 all_modules_ids     = []
-
 for line in open("kegg_terms_per_module.tsv", "r"): 
    md_id = line.split("\t")[0]
    if md_id not in all_modules_ids: 
       all_modules_ids.append(md_id)
 
-print("# of modules: ", len(all_modules_ids))
 
 for md in all_modules_ids:
    
-   print(md)
-
    url       = "http://rest.kegg.jp/get/" + md
    response   = requests.get(url)
 
@@ -60,13 +68,4 @@ for md in all_modules_ids:
          write_definition(line, md, num_of_defs)
    
    time.sleep(0.5)
-         
-
-
-
-# Definition	The definition of the module as a logcail expression of K numbers. 
-# Comma separated K numbers indicate alternatives. 
-# Plus signs are used to represent a complex and a minus sign denotes a non-essential component in the complex.
-
-
 
